d9ab11dd0f4d4c2221bb2829928283570210864c
Piorosen <chacha@udon.party>
2023-11-27 15:16:04 +0000

    add: installer code

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -z $(which mpstat) ]; then
+    git clone git://github.com/sysstat/sysstat
+    cd sysstat && ./configure
+    make && sudo make install
+fi
+
+mkdir -p ~/.local/bin
+
+
+curl -fsSL https://raw.githubusercontent.com/Piorosen/mpstat2csv/main/mpstat2csv > ~/.local/bin/mpstat2csv && \
+    chmod +x ~/.local/bin/mpstat2csv
+
+
